b99cd8306f642938f74f3f9547659800a1f0d6ff
Arieljam <arieljam.cba@gmail.com>
2025-10-02 15:12:53 -0300

    Create UserProfile.java

---
+++ app/src/main/java/com/example/eros/UserProfile.java
@@ -0,0 +1,13 @@
+package com.example.eros;
+
+public class UserProfile {
+    String name;
+    int age;
+    String imageUrl;
+
+    public UserProfile(String name, int age, String imageUrl) {
+        this.name = name;
+        this.age = age;
+        this.imageUrl = imageUrl;
+    }
+}
