ba6be985dd46fc7845cea0d363bf62f1cb3d5fbd
Arieljam <arieljam.cba@gmail.com>
2025-10-02 15:30:52 -0300

    Create Message.java

---
+++ app/src/main/java/com/example/eros/Message.java
@@ -0,0 +1,11 @@
+package com.example.eros;
+
+public class Message {
+    String sender;
+    String text;
+
+    public Message(String sender, String text) {
+        this.sender = sender;
+        this.text = text;
+    }
+}
