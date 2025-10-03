3234e17dcc58b080cc20874f8db02f5617ed96ac
Arieljam <arieljam.cba@gmail.com>
2025-10-03 14:22:03 -0300

    Update UserProfile.java

--- app/src/main/java/com/example/eros/UserProfile.java
+++ app/src/main/java/com/example/eros/UserProfile.java
@@ -1,13 +1,37 @@
 package com.example.eros;
 
 public class UserProfile {
-    String name;
-    int age;
-    String imageUrl;
+    private String id;
+    private String name;
+    private int age;
+    private String bio;
+    private String photoUrl;
 
-    public UserProfile(String name, int age, String imageUrl) {
+    public UserProfile(String id, String name, int age, String bio, String photoUrl) {
+        this.id = id;
         this.name = name;
         this.age = age;
-        this.imageUrl = imageUrl;
+        this.bio = bio;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
     }
 }
